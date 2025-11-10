4c5f07d44200a77b811407b8f5d4d34a986bc70a
divapuspitaa <divapuspita627@gmail.com>
2025-11-10 20:43:49 +0800

    ADD: CekMood

---
+++ CekMood.py
@@ -0,0 +1,12 @@
+def cek_mood(): 
+    print("=== How Are You Today? ===")
+    print("Selamat Datang Diprogram cek suasana hati Anda hari ini😇🤩")
+    print("Jawab setiap pertanyaan dengan daftar opsi (1-5)")
+    print("1. Sangat buruk")
+    print("2. Buruk")
+    print("3. Biasa saja")
+    print("4. Baik")
+    print("5. Sangat baik\n")
+
+cek_mood ()
+
