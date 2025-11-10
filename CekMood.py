847134334798006e74d3cb1c02df6d7d77f0a455
divapuspitaa <divapuspita627@gmail.com>
2025-11-10 20:49:29 +0800

    UPDATE: CekMood

--- CekMood.py
+++ CekMood.py
@@ -1,4 +1,21 @@
 def cek_mood(): 
+    pertanyaan = [
+        "1. Bagaimana suasana hatimu secara keseluruhan saat ini?",
+        "2. Seberapa tenang dan nyaman perasaanmu saat ini?",
+        "3. Seberapa stabil emosimu setelah melalui hari ini?",
+        "4. Bagaimana tingkat stres atau ketegangan yang kamu rasakan saat ini?",
+        "5. Seberapa puas kamu dengan hari yang kamu jalani hari ini?"
+    ]
+    
+    daftar_opsi = [
+        "   1. Sangat buruk",
+        "   2. Buruk",
+        "   3. Biasa saja",
+        "   4. Baik",
+        "   5. Sangat baik"
+    ]
+    
+    jawaban = [] 
     print("=== How Are You Today? ===")
     print("Selamat Datang Diprogram cek suasana hati Anda hari ini😇🤩")
     print("Jawab setiap pertanyaan dengan daftar opsi (1-5)")
@@ -7,6 +24,23 @@ def cek_mood():
     print("3. Biasa saja")
     print("4. Baik")
     print("5. Sangat baik\n")
+
+    for i in pertanyaan:
+        print(i)
+        for x in daftar_opsi:
+            print(x)
+        while True:
+            try:
+                opsi = int(input("Masukkan opsi (1-5) : "))
+                if opsi >= 1 and opsi <= 5:
+                    jawaban.append(opsi)
+                    break
+                else:
+                    print("Input hanya boleh memasukkan angka 1-5. Silahkan masukkan angka 1-5")
+            except ValueError:
+                    print("Input tidak valid. Silahkan masukkan angka 1-5")  
+
+        print()
 
 cek_mood ()
 
