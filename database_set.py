0d655c8e71db0b7864c8752f9a0458ac67b888b5
arenjiee <moskovwtf@gmail.com>
2025-11-10 21:07:58 +0800

    UPDATE: database_set

--- database_set.py
+++ database_set.py
@@ -20,4 +20,35 @@ CREATE TABLE IF NOT EXISTS workouts (
 )
 """)
 
+
 conn.commit()
+
+cursor.execute("SELECT COUNT(*) FROM targets")
+if cursor.fetchone()[0] == 0:
+    cursor.execute("INSERT INTO targets (id, kalori, protein) VALUES (1, 0, 0)")
+    conn.commit()
+
+
+def insert_target_workout(workout):
+    cursor.execute("INSERT INTO workouts (nama_workout) VALUES (?)", (workout,))
+    conn.commit()
+
+def delete_workout_by_id(workout_id):
+    cursor.execute("DELETE FROM workouts WHERE id = ?", (workout_id,))
+    conn.commit()
+
+def insert_target_kalori(kalori):
+    cursor.execute("UPDATE targets SET kalori = ?", (kalori,))
+    conn.commit()
+
+def insert_target_protein(protein):
+    cursor.execute("UPDATE targets SET protein = ?", (protein,))
+    conn.commit()
+
+def get_target():
+    cursor.execute("SELECT kalori, protein FROM targets WHERE id = 1")
+    return cursor.fetchone()
+
+def get_workouts():
+    cursor.execute("SELECT id, nama_workout FROM workouts ORDER BY id")
+    return cursor.fetchall()
